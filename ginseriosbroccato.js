cdcc667a3335bf5bb0073749b21f232d37fc31a5
GabrieleFoti <60048589+GabrieleFoti@users.noreply.github.com>
2021-11-04 15:23:26 +0100

    Update ginseriosbroccato.js

--- ginseriosbroccato.js
+++ ginseriosbroccato.js
@@ -337,7 +337,10 @@ client.on('message', async message => {
     msg.shift()
     if(msg == undefined) return message.reply("you must specify a message first.")
     connection.query(`UPDATE Guild SET welcome_msg_content = ${msg}, welcome_channel_id = NULL WHERE guild_id = ${message.guild.id}`, (err) => {
-        if(err) message.reply('there was an error setting the message, please try again.')
+        if(err){ 
+          message.reply('there was an error setting the message, please try again.')
+          console.log(err)
+        }
     })
   }else if(message.content.startsWith(prefix + 'autorole')){
     if(!message.member.permissions.has('MANAGE_GUILD')) return message.reply('you do not have the permission to perform this action.')
